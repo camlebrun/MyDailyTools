4f9abaef620c601f0a6d6f432d5b1f370d0ff76a
camlebrun <c_lebrun3@hetic.eu>
2023-01-30 18:51:31 +0100

    prendre le cas soustraire dans le print

--- app.py
+++ app.py
@@ -16,4 +16,8 @@ st.write('Vous avez choisi le : ', d)
 operation = st.selectbox("Sélectionnez l'opération", ["Ajouter", "Soustraire"])
 days_to_add = st.number_input("Choisir le nombre de jour à ajouter/soustraire", min_value=0, step=1)
 d2 = calculate_date(operation, days_to_add, d)
-st.write('La date avec le nombre de jours ajoutés est : ', d2)
+if operation == "Ajouter":
+    st.write('La date avec le nombre de jours ajoutés est : ', d2)
+elif operation == "Soustraire":
+    st.write('La date avec le nombre de jours soustraits est : ', d2)
+
