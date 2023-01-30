5089f24cc93b8fecb91475dbfd4b1e0e530e722f
camlebrun <c_lebrun3@hetic.eu>
2023-01-30 20:50:11 +0100

    change password

--- tool_app/pages/Mots_de_passe_forts.py
+++ tool_app/pages/Mots_de_passe_forts.py
@@ -5,13 +5,13 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title="Mots de passe forts")
 st.markdown("<h1 style='text-align: center'>Mots de passe forts</h1>", unsafe_allow_html=True)
 
-
+password_type = st.selectbox("Type de caractères :",
+    ["Lettres et chiffres", "Tous les caractères"])
 length = st.slider("Taille du mot de passe :", min_value=8, max_value=32, value=12, step=1)
 
 chars = string.ascii_letters + string.digits + string.punctuation
 
-password_type = st.selectbox("Type de caractères :",
-    ["Lettres et chiffres", "Tous les caractères"])
+
 
 if password_type == "Tous les caractères":
     chars = string.ascii_letters + string.digits + string.punctuation
