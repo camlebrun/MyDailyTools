167690510f22f101cc07164f8c248b44dcfefbac
camlebrun <c_lebrun3@hetic.eu>
2023-02-09 18:17:15 +0100

    resolution probléme année

--- tool_app/pages/2_Difference_date.py
+++ tool_app/pages/2_Difference_date.py
@@ -1,42 +1,37 @@
 import streamlit as st
-from datetime import datetime, timedelta
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 st.markdown("<h1 style='text-align: center'>Calcul de différence de dates</h1>", unsafe_allow_html=True)
-
-def format_timedelta(delta):
-    if delta.days < 0:
-        return 
-    if delta.days == 0:
-        return 
-    if delta.days == 1:
-        return "1 jour"
-    else:
-        days = delta.days
-        months = days // 30
-        years = months // 12
-        months = months % 12
-        days = days % 30
-        if years == 0:
-            if months == 0:
-                return f"{days} jours"
-            else:
-                return f"{months} mois et {days} jours"
-        else:
-            return f"{years} ans,  {months} mois, et {days} jours"
-
 
 def date_difference():
     start_date = st.date_input("Entrez la date de départ :")
     end_date = st.date_input("Entrez la date de fin :")
-    delta = end_date - start_date
-    if delta.days < 0:
-        st.warning("La date de départ doit être antérieure à la date de fin.")
-    elif delta.days == 0:
-        st.warning("Les deux dates sont identiques.")
-    else:
-        delta_str = format_timedelta(delta)
-        st.metric("Différence :", delta_str)
+    delta = relativedelta(end_date, start_date)
+    years = delta.years
+    months = delta.months
+    days = delta.days
 
+    result = ""
+
+    if years == 0 and months == 0 and days == 0:
+        result = "Les deux dates sont identiques."
+    elif years < 1 and months < 1 and days <1:
+        result = "La date de fin est antérieure à la date de départ."
+    if years == 1:
+        result += "1 an "
+    elif years > 1:
+        result += f"{years} ans "
+    if months == 1:
+        result += "1 mois "
+    elif months > 1:
+        result += f"{months} mois "
+    if days == 1:
+        result += "1 jour"
+    elif days > 1:
+        result += f"{days} jours"
+
+    st.metric("Résultat :", result)
 
 if __name__ == '__main__':
     date_difference()
