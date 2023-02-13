94b0021b5b2c19e197fffae4fa0526b0a0d3dd06
camlebrun <c_lebrun3@hetic.eu>
2023-02-13 14:54:47 +0100

    new versions

--- tool_app/pages/4Mots_de_passe_forts.py
+++ tool_app/pages/4Mots_de_passe_forts.py
@@ -5,13 +5,16 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title="Mots de passe forts")
 st.markdown("<h1 style='text-align: center'>Mots de passe forts</h1>", unsafe_allow_html=True)
 
-password_type = st.selectbox("Type de caractères :",
-    [ "Tous les caractères", "Lettres et chiffres"])
-length = st.slider("Taille du mot de passe :", min_value=8, max_value=32, value=12, step=1)
-
 chars = string.ascii_letters + string.digits + string.punctuation
 
+col1, col2 = st.columns(2)
 
+with col1:
+    password_type = st.selectbox("Type de caractères :",
+        [ "Tous les caractères", "Lettres et chiffres"])
+
+with col2:
+    length = st.number_input("Taille du mot de passe :", min_value=0,max_value=32, value=12,  step=1)
 
 if password_type == "Tous les caractères":
     chars = string.ascii_letters + string.digits + string.punctuation+ string.punctuation
@@ -26,7 +29,6 @@ password = generate_password(length, chars)
 
 st.write("Voici votre mot de passe fort :")
 code = st.code(password, language="text")
-
 
 def refresh():
     st.write("Page actualisée!")
